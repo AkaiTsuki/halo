71be6503033222c166378f949bbbfc3c5d15bcad
akaitsuki <liu.jiach@husky.neu.edu>
2019-12-15 21:07:46 +0800

    fix bugs

--- halo-app/src/main/java/io/akaitsuki/halo/app/controller/MainController.java
+++ halo-app/src/main/java/io/akaitsuki/halo/app/controller/MainController.java
@@ -1,15 +1,12 @@
 package io.akaitsuki.halo.app.controller;
 
 import io.akaitsuki.halo.app.extension.customer.CustomerNameExt;
-import io.akaitsuki.halo.app.extension.customer.UserCustomerNameExtImpl;
-import io.akaitsuki.halo.core.BizCoordinator;
+import io.akaitsuki.halo.core.BizScenario;
 import io.akaitsuki.halo.core.ExtensionPointExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 @RestController
 public class MainController {
@@ -26,11 +23,11 @@ public class MainController {
     @GetMapping("/cutomerName/{userType}")
     public String getCustomerName(@PathVariable("userType") String userType) {
 
-        BizCoordinator bizCoordinator = new BizCoordinator();
-        bizCoordinator.setBizId("biz1");
-        bizCoordinator.setUseCase("getCustomerName");
-        bizCoordinator.setScenario(userType);
+        BizScenario bizScenario = new BizScenario();
+        bizScenario.setBizId("biz1");
+        bizScenario.setUseCase("getCustomerName");
+        bizScenario.setScenario(userType);
 
-        return extensionPointExecutor.execute(CustomerNameExt.class, bizCoordinator, extension -> extension.customerName("John", "Clark"));
+        return extensionPointExecutor.execute(CustomerNameExt.class, bizScenario, extension -> extension.customerName("John", "Clark"));
     }
 }
